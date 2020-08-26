8b44a7a57f5b895b6ae40239cf5d3fbcb4948c99
Shamali Patil <shamalirpatil@gmail.com>
2020-08-26 09:56:21 +0100

    Initial Commit

---
+++ ByteCoinApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteCoinApp
+//
+//  Created by Shamali Patil on 26/08/2020.
+//  Copyright © 2020 Shamali Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
